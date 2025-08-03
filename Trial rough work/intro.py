07f041c9742246a12a387a318009e545285eb089
tonygachie <anthonygachie95@gmail.com>
2025-08-03 13:32:16 +0300

    My first portfolio project

---
+++ Trial rough work/intro.py
@@ -0,0 +1,22 @@
+PLP = "Academy"
+Student_Name = "Joy Kamau"
+Student_ID = 1001
+Student_Age = 20
+Fees_clearance = True
+
+print(Student_Name,Student_ID, Student_Age)
+
+languages= ["joy", "cool","jonte","mwizi"]
+print(languages)
+#tuples
+products = ('xbox',499.99,'habibi')
+print(products)
+#sets
+Set_A = {1,2,3,4,5}
+Set_B = {6,7,8,9,}
+print(Set_A)
+print(Set_B)
+
+#dictunaries
+Student_info = {"name": "john", "age":120}
+print(Student_info)
